2e04e065a4cccc4a82eb3255b7a5f555c5756f4a
afhappy <xiao0423fei@163.com>
2018-03-27 16:26:27 +0800

    Initial commit

---
+++ Example/AFSwiftExtensionKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AFSwiftExtensionKit
+//
+//  Created by anandevelope on 03/27/2018.
+//  Copyright (c) 2018 anandevelope. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
